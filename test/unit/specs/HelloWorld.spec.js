ce2ae70a19a4c78f25a96be6d1f44c0f7d8f46b0
吴保粮 <trrymail@163.com>
2020-10-26 10:03:43 +0800

    删除图片

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,18 @@
+/*
+ * @Author: trry
+ * @Date: 2020-10-22 15:41:17
+ * @LastEditors: trry
+ * @LastEditTime: 2020-10-26 09:10:20
+ * @Description: file content
+ */
 import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
-  it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .toEqual('Welcome to Your Vue.js App')
-  })
+    it('should render correct contents', () => {
+        const Constructor = Vue.extend(HelloWorld)
+        const vm = new Constructor().$mount()
+        expect(vm.$el.querySelector('.hello h1').textContent)
+            .toEqual('Welcome to Your Vue.js App')
+    })
 })
